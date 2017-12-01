89839bcbc65751e03c3925163064aa76ce0f4826
SeanA <sean.arca@northwestern.edu>
2017-12-01 15:23:48 -0600

    choose random word and split into letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 // Set variables
 var wordBank = ["Daenarys", "Dragons", "White Walkers", "John Snow", "Red Wedding"];
+var wordChosen = [];
 var guesses = 6;
 var userText = document.getElementById("entered");
 var userTry = document.getElementById("tries-left");
@@ -8,11 +9,15 @@ var userTry = document.getElementById("tries-left");
 // List the words available in the wordbank
 for (var i = 0; i < wordBank.length; i++) {
     
-
-	var wordChoice = wordBank[i];
+	// Choose a word at random
+	var wordChoices = wordBank[Math.floor(Math.random() * wordBank.length)];
+	console.log(wordChoices);
 
 	// Split the word chosen
-    var wordSplit = wordChoice.split("");
+    var wordSplit = wordChoices.split("");
+    console.log(wordSplit);
+
+    // For every letter, replace with "_"
 }
 
 
@@ -22,11 +27,14 @@ document.onkeyup = function(event) {
 	// Determines which key is pressed
     userText.textContent = event.key;
 
-    // Push the letter used into an array and display
+    // Push the letter used into an array and display .push()
 
     // Display how many attempts left (use attempts div)
 
 };
-    
+
+// Function to 
+
+
 
 // Function to go to next word when word is guessed (use confirm?)
