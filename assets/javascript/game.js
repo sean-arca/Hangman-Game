3d65663cb0115c205d299ae69c7bee181286ef02
SeanA <sean.arca@northwestern.edu>
2017-12-04 10:16:05 -0600

    added animate.css to text, added Game Design comments to js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,11 @@
 // Set variables
 var wordBank = ["Daenarys", "Dragons", "White Walkers", "John Snow", "Red Wedding"];
-var wordChosen = [];
+var wordLetters = [];
+var letterChosen = [];
 var guesses = 6;
 var userText = document.getElementById("entered");
+var wordChosen = document.getElementById("guessWord");
 var userTry = document.getElementById("tries-left");
-
-
-// List the words available in the wordbank
-for (var i = 0; i < wordBank.length; i++) {
-    
-	// Choose a word at random
-	var wordChoices = wordBank[Math.floor(Math.random() * wordBank.length)];
-	console.log(wordChoices);
-
-	// Split the word chosen
-    var wordSplit = wordChoices.split("");
-    console.log(wordSplit);
-
-    // For every letter, replace with "_"
-}
 
 
 // Run this function when a key is pressed
@@ -27,14 +14,53 @@ document.onkeyup = function(event) {
 	// Determines which key is pressed
     userText.textContent = event.key;
 
+	// List the words available in the wordbank
+	for (var i = 0; i < wordBank.length; i++) {
+    
+		// Choose a word at random
+		var wordChoices = wordBank[Math.floor(Math.random() * wordBank.length)];
+		console.log(wordChoices);
+
+		// Split the word chosen and push into array
+    	var wordSplit = wordChoices.split("");
+    	wordLetters.push(wordSplit);
+    	wordChosen.innerText = wordLetters
+    	console.log(wordSplit);
+
+   		// For every letter, replace with "_" Look at drinkslist example for createElement (use span on html for "_")
+
+   		//Subtract a Guess
+   		guesses--
+	}
+};
+
+
+
     // Push the letter used into an array and display .push()
 
     // Display how many attempts left (use attempts div)
 
-};
+
 
 // Function to 
 
 
 
 // Function to go to next word when word is guessed (use confirm?)
+
+
+
+/* Game Design:
+
+1. Pick a random word
+2. Take the player's guess
+3. Quit the game if player wants to
+4. Check that the player's guess is a valid answer
+5. Keep track of letters guessed
+6. Show player progress
+7. Finish when player guessed the word
+
+
+
+
+
