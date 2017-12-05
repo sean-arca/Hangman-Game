914d8057d09e68eb61a41d6507984b37f54bc156
SeanA <sean.arca@northwestern.edu>
2017-12-04 21:42:43 -0600

    on key up = push letter into array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,11 +22,24 @@ $("#guessWord").append(placeHolders);
 
 // Target where to display letterschosen(on key up)
 var lettersChosen = [];
-var userText = document.getElementById("entered");
 
 // Run this function when key is pressed
-document.onkeyup = function(event) {
-	userText.innerHTML = event.key;
-	lettersChosen.push(userText);
-	// $("#entered").append(lettersChosen);
+document.onkeydown = function(event) {
+
+	// Display letter pressed in div id [NEED TO CHANGE TO DISPLAY ARRAY]
+	document.getElementById("entered").innerHTML = event.key;
+	// Push letter pressed into letterChosen array
+	lettersChosen.push(event.key);
+
+	// If/else if letter pressed matches word
+
+		// (If) match then change placeholder text to letter
+
+		// (Else If) no match subtract from chances left and display on #tries-left span
+
+		// (Else) lose if lives = 0
 }
+
+
+
+
