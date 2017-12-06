767bf540a4660520c8dbe62db738ee5d8a4d23b5
Sean Arca <Sean@dhcp-10-105-85-43.wireless.northwestern.private>
2017-12-06 10:55:38 -0600

    winning/losing conditions added, event.key displays properly on word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,10 +15,6 @@ for (var i = 0; i < word.length; i++) {
 	ourWord.push(word[i]);
 	// Push the amount of ${word}'s letters into the ${placeHolders} array but replaces it with "_ ".
 	placeHolders[i] = "_ ";
-	// Winning condition
-	if (placeHolders.indexOf("_ ") === -1) {
-		document.write(`<h1 style="font-family:Playfair Display; font-size:3em; font-weight:200; position:absolute; right:100px; top:100px">You heard ${ourWord}.</h1>`);
-	}
 }
 
 // Append or Insert the content of the ${placeHolders} array into the #guessWord element.
@@ -37,40 +33,61 @@ var lettersRemain = word.length;
 document.onkeyup = function (event) {
 
 	// Display letter pressed in #entered div
-	document.getElementById("entered").innerHTML = lettersChosen;
 	lettersChosen.push(event.key);
+	document.getElementById("entered").innerHTML = lettersChosen;	
 	console.log("Key Pressed");
 	console.log(lettersChosen);
 	
 	// For loop to do comparison instead
-	for (var j = 0; j < ourWord.length; j++) {
+	for (var position = 0; position < ourWord.length; position++) {
 		
-
-		if (ourWord[j] === lettersChosen[j]) {
-			placeHolders[i] = "";
+		if (ourWord[position] === event.key) {
+			placeHolders[position] = event.key;
+			document.getElementById("guessWord").innerHTML = placeHolders.join("");
 			lettersRemain--;
-		};
-		
+		}
 	}
 
 
 
-	// // If/else if letterChosen matches ourWord array
-	// if (lettersChosen.indexOf() === ourWord.indexOf()) {
-	// 	// (If) match then change placeholder text to letter (user indexOf)
-	// 	console.log("That letter matches");
+	var isWrong = function () {
+		if (ourWord[position] !== event.key) {
+			alert("wrong!");
+		}	
+	}
 
-	// }
 
+
+	// else if (lettersChosen[i] !== ourWord[i]) {
+	// 	
+	// 	console.log("Not a match");
+
+	// if no letters match, by the end of the loop subtract from chances left
+	// .join to get rid of commas
+
+	// // }
+	// lettersRemain--;
 	// If no match subtract from chances left and display on #tries-left span
-	if (lettersChosen[i] !== ourWord[i]) {
-		triesLeft.innerHTML--;
-		tries--;
-		console.log("Not a match");
+	var notMatch;
+
+	// Game Reset
+	
+
+	// Winning condition
+	if (lettersRemain === 0) {
+		alert("You heard " + ourWord.join(""));
+
+		var playAgain = confirm("Do you hear something else?");
+		if (playAgain === true) {
+			window.location.reload(true);
+		}
 	}
+
+	
+
 
 	// (If) lose if lives = 0
-	if (tries === 0 && lettersRemain >= 1) {
+	if (tries <= 0 && lettersRemain >= 1) {
 		document.write(`<h1 style="font-family:Playfair Display; font-size:3em; font-weight:200; position:absolute; right:100px; top:100px">You lose consciousness for a moment and suddenly..</h1><br><img src="./assets/images/end.gif" style="position:absolute; right:100px; top: 300px; width: 500px; height:auto;">`);
 		setTimeout(function() {
 			window.location.reload(true);
